766ba9f77efbe1af7f07fb9ae6e28bc90a81c178
Andre Behrens <andre.behrens@haw-hamburg.de>
2014-05-28 01:17:17 +0200

    Mps Socket

--- src/main/java/bai5_hibernate/MPS_AE/App.java
+++ src/main/java/bai5_hibernate/MPS_AE/App.java
@@ -1,13 +1,112 @@
 package bai5_hibernate.MPS_AE;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+
+public class App {
+
+	private final AppTest appTest;
+
+	private final ExecutorService executorService = Executors
+			.newFixedThreadPool(3);
+
+	private final ScheduledExecutorService scheduledExecutor = Executors
+			.newScheduledThreadPool(3);
+
+	private ServerSocket socket;
+
+	public App() {
+		this.appTest = new AppTest();
+		listen();
+	}
+
+	public void listen() {
+		try {
+			socket = new ServerSocket(0);
+			while (true) {
+				// Incoming client requests
+				System.out.println(String.format(
+						"MPS Instance listening @ Port:%s",
+						socket.getLocalPort()));
+				final Socket connection = socket.accept();
+				executorService.execute(new Runnable() {
+
+					private BufferedReader inStream;
+
+					public void run() {
+						try {
+							inStream = new BufferedReader(
+									new InputStreamReader(connection
+											.getInputStream()));
+							JsonObject request = Json.createReader(
+									new StringReader(inStream.readLine()))
+									.readObject();
+							String plainRequest = request.getString("request");
+							if (plainRequest.contains("doSo")) {
+								appTest.test1();
+							} else if (plainRequest.contains("shutdown")) {
+								executorService.shutdown();
+								scheduledExecutor.shutdown();
+							} else {
+								System.err.println(plainRequest);
+							}
+						} catch (IOException e) {
+							e.printStackTrace();
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+
+					}
+				});
+				scheduledExecutor.schedule(new Runnable() {
+
+					private final DataOutputStream outStream = new DataOutputStream(
+							connection.getOutputStream());
+
+					public void run() {
+						try {
+							JsonObject theObject = Json
+									.createObjectBuilder()
+									.add("response", "add")
+									.add("key",
+											connection.getInetAddress()
+													.getHostName())
+									.add("data",
+											Json.createObjectBuilder()
+													.add("status", "on")
+													.add("uptime", "100")
+													.add("requests", "0")
+													.add("systemLoad", "0"))
+									.build();
+
+							outStream.writeBytes(theObject.toString() + "\n");
+						} catch (UnknownHostException e) {
+							e.printStackTrace();
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+					}
+				}, 5, TimeUnit.SECONDS);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		new App();
+	}
 }
---
+++ src/main/java/bai5_hibernate/MPS_AE/AppTest.java
@@ -0,0 +1,164 @@
+package bai5_hibernate.MPS_AE;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Assert;
+
+import bai5_hibernate.MPS_AE.hibernate.dao.AngebotDao;
+import bai5_hibernate.MPS_AE.hibernate.dao.ArbeitsplanDao;
+import bai5_hibernate.MPS_AE.hibernate.dao.AuftragDao;
+import bai5_hibernate.MPS_AE.hibernate.dao.BauteilDao;
+import bai5_hibernate.MPS_AE.hibernate.dao.FertigungsauftragDao;
+import bai5_hibernate.MPS_AE.hibernate.dao.KundeDao;
+import bai5_hibernate.MPS_AE.hibernate.dao.LieferungDao;
+import bai5_hibernate.MPS_AE.hibernate.dao.RechnungDao;
+import bai5_hibernate.MPS_AE.hibernate.dao.StuecklisteDao;
+import bai5_hibernate.MPS_AE.hibernate.dao.TransportauftragDao;
+import bai5_hibernate.MPS_AE.hibernate.dao.VorgangDao;
+import bai5_hibernate.MPS_AE.hibernate.dao.ZahlungseingangDao;
+import bai5_hibernate.MPS_AE.hibernate.tables.Arbeitsplan;
+import bai5_hibernate.MPS_AE.hibernate.tables.Auftrag;
+import bai5_hibernate.MPS_AE.hibernate.tables.Bauteil;
+import bai5_hibernate.MPS_AE.hibernate.tables.Fertigungsauftrag;
+import bai5_hibernate.MPS_AE.hibernate.tables.Vorgang;
+import bai5_hibernate.MPS_AE.hibernate.tables.VorgangArtTyp;
+
+public class AppTest {
+	protected AuftragDao auftragDao;
+	protected AngebotDao angebotDao;
+	protected ArbeitsplanDao arbeitsplanDao;
+	protected BauteilDao bauteilDao;
+	protected FertigungsauftragDao fertigungsauftragDao;
+	protected KundeDao kundeDao;
+	protected LieferungDao lieferungDao;
+	protected RechnungDao rechnungDao;
+	protected StuecklisteDao stuecklisteDao;
+	protected TransportauftragDao transportauftragDao;
+	protected VorgangDao vorgangDao;
+	protected ZahlungseingangDao zahlungseingangDao;
+
+	public AppTest() {
+		init();
+	}
+
+	public void init() {
+		this.auftragDao = new AuftragDao();
+		this.angebotDao = new AngebotDao();
+		this.arbeitsplanDao = new ArbeitsplanDao();
+		this.bauteilDao = new BauteilDao();
+		this.fertigungsauftragDao = new FertigungsauftragDao();
+		this.kundeDao = new KundeDao();
+		this.lieferungDao = new LieferungDao();
+		this.rechnungDao = new RechnungDao();
+		this.stuecklisteDao = new StuecklisteDao();
+		this.transportauftragDao = new TransportauftragDao();
+		this.vorgangDao = new VorgangDao();
+		this.zahlungseingangDao = new ZahlungseingangDao();
+	}
+
+	private Fertigungsauftrag fertigungsauftrag;
+	private Bauteil bauteil;
+	private Arbeitsplan arbeitsplan;
+	private Vorgang vorgang;
+	private Auftrag auftrag;
+
+	public void test1() throws Exception {
+		auftrag = new Auftrag();
+		auftrag.setBeauftragtAm(new Date());
+		auftrag.setIstAbgeschlossen(false);
+		auftrag.setFertigungsauftrag(getFertigungsauftrag(auftrag));
+
+		auftragDao.add(auftrag);
+
+		fertigungsauftrag.setAuftrag(auftrag);
+		fertigungsauftragDao.update(fertigungsauftrag);
+
+		Assert.assertNotNull(auftrag.getNummer());
+	}
+
+	private Fertigungsauftrag getFertigungsauftrag(Auftrag auftrag)
+			throws Exception {
+		fertigungsauftrag = new Fertigungsauftrag();
+		fertigungsauftrag.setBauteil(getBauteil(fertigungsauftrag));
+
+		fertigungsauftragDao.add(fertigungsauftrag);
+		return fertigungsauftrag;
+	}
+
+	private Bauteil getBauteil(Fertigungsauftrag fertigungsauftrag)
+			throws Exception {
+		bauteil = new Bauteil();
+		bauteil.setArbeitsplan(getArbeitsplan(bauteil));
+		bauteil.setName("Erstes Bauteil");
+
+		bauteilDao.add(bauteil);
+		bauteil.addFertigungsauftrag(fertigungsauftrag);
+		bauteilDao.update(bauteil);
+
+		arbeitsplan.setBauteil(bauteil);
+		arbeitsplanDao.update(arbeitsplan);
+
+		return bauteil;
+	}
+
+	private Arbeitsplan getArbeitsplan(Bauteil bauteil) throws Exception {
+		arbeitsplan = new Arbeitsplan();
+		arbeitsplan.setVorgangsListe(getVorgaenge(arbeitsplan));
+
+		arbeitsplanDao.add(arbeitsplan);
+
+		vorgang.setArbeitsplan(arbeitsplan);
+		vorgangDao.update(vorgang);
+
+		return arbeitsplan;
+	}
+
+	private Set<Vorgang> getVorgaenge(Arbeitsplan arbeitsplan) throws Exception {
+		HashSet<Vorgang> vorgangsliste = new HashSet<Vorgang>();
+		vorgang = new Vorgang();
+		vorgang.setArt(VorgangArtTyp.BEREITSTELLUNG);
+
+		vorgangDao.add(vorgang);
+		vorgangsliste.add(vorgang);
+		return vorgangsliste;
+	}
+
+	public void testFertigungArtikel() throws Exception {
+		auftragErstellen();
+
+		auftrag = auftragDao.findById(auftrag.getNummer());
+		arbeitsplan = auftrag.getFertigungsauftrag().getBauteil()
+				.getArbeitsplan();
+
+		Vorgang vorgang2 = null;
+		for (Vorgang vorgang : arbeitsplan.getVorgangsListe()) {
+			vorgang.setArt(VorgangArtTyp.MONTAGE);
+			vorgangDao.update(vorgang);
+
+			vorgang2 = vorgang;
+		}
+
+		Assert.assertSame(VorgangArtTyp.MONTAGE,
+				vorgangDao.findById(vorgang2.getNummer()).getArt());
+	}
+
+	public void testComplex() throws Exception {
+		Bauteil bauteil = bauteilDao.findById(1l);
+
+		Assert.assertFalse(bauteilDao.isComplex(bauteil));
+	}
+
+	private void auftragErstellen() throws Exception {
+		auftrag = new Auftrag();
+		auftrag.setBeauftragtAm(new Date());
+		auftrag.setIstAbgeschlossen(false);
+		auftrag.setFertigungsauftrag(getFertigungsauftrag(auftrag));
+
+		auftragDao.add(auftrag);
+
+		fertigungsauftrag.setAuftrag(auftrag);
+		fertigungsauftragDao.update(fertigungsauftrag);
+	}
+}
